b5b7496f3529b27ecdef3164ebc8a37689efd4b1
clin <78265547+bannalol@users.noreply.github.com>
2025-11-24 14:26:16 -0500

    update backend app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 console.log(">>> APP.JS EXECUTED <<<");
 console.log("Loaded USDA key:", process.env.USDA_KEY);
-
 
 import createError from "http-errors";
 import express, { json, urlencoded } from "express";
@@ -13,23 +11,18 @@ import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
-
 // Routers
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 import usdaRouter from "./routes/usda.js";
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const app = express();
-
 
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "hbs");
-
 
 app.use(logger("dev"));
 app.use(json());
@@ -37,16 +30,13 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api/usda", usdaRouter);
 
-
 app.use((req, res, next) => {
  next(createError(404));
 });
-
 
 app.use((err, req, res, next) => {
  res.locals.message = err.message;
@@ -55,7 +45,6 @@ app.use((err, req, res, next) => {
  res.status(err.status || 500);
  res.render("error");
 });
-
 
 export default app;
 
